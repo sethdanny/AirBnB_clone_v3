cfea68a012666c54fe777559c5d7d5dc481dc8c9
sethdanny <naddulidaniel1994@gmail.com>
2023-04-28 14:47:37 -0700

    updated file storage and db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ returns an object based on the class and its id
+        or None, if not found """
+        obj = None
+        if cls is not None and issubclass(cls, BaseModel):
+            obj = self.__session.query(cls).filter(cls.id == id).first()
+            return obj
+
+    def count(self, cls=None):
+        """ returns the number of objects in storage of a given class """
+        return len(self.all(cls))
